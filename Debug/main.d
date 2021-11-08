38a517aec4aa6c41f6ec5f5af77a0eee7b332be8
Aum Patel <aumpatel16@gmail.com>
2021-11-07 23:39:36 -0500

    Uploaded from archive

---
+++ Debug/main.d
@@ -0,0 +1,33 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stdio.h
+main.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/_ti_config.h
+main.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/linkage.h
+main.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stdarg.h
+main.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/_types.h
+main.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/cdefs.h
+main.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/machine/_types.h
+main.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/string.h
+main.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/xlocale/_string.h
+
+../main.c:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stdio.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/_ti_config.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/linkage.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stdarg.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/_types.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/machine/_types.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/string.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/xlocale/_string.h:
+
